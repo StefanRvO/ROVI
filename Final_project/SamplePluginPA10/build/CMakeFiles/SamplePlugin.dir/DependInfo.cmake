38d3b309b6d04b417aa79e36ce2d768ec1913dd0
Stefan Ravn van Overeem <stefan@stefanrvo.dk>
2017-03-16 16:50:30 +0100

    Experimenting with adding Docker support..

---
+++ Final_project/SamplePluginPA10/build/CMakeFiles/SamplePlugin.dir/DependInfo.cmake
@@ -0,0 +1,51 @@
+# The set of languages for which implicit dependencies are needed:
+SET(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+SET(CMAKE_DEPENDS_CHECK_CXX
+  "/home/student/stefan_home/SamplePluginPA10/build/qrc_resources.cxx" "/home/student/stefan_home/SamplePluginPA10/build/CMakeFiles/SamplePlugin.dir/qrc_resources.cxx.o"
+  "/home/student/stefan_home/SamplePluginPA10/src/SamplePlugin.cpp" "/home/student/stefan_home/SamplePluginPA10/build/CMakeFiles/SamplePlugin.dir/src/SamplePlugin.cpp.o"
+  "/home/student/stefan_home/SamplePluginPA10/build/src/moc_SamplePlugin.cxx" "/home/student/stefan_home/SamplePluginPA10/build/CMakeFiles/SamplePlugin.dir/src/moc_SamplePlugin.cxx.o"
+  )
+SET(CMAKE_CXX_COMPILER_ID "GNU")
+
+# Preprocessor definitions for this target.
+SET(CMAKE_TARGET_DEFINITIONS
+  "BOOST_DISABLE_ASSERTS"
+  "QT_CORE_LIB"
+  "QT_DESIGNER_LIB"
+  "QT_GUI_LIB"
+  "QT_NO_DEBUG"
+  "QT_OPENGL_LIB"
+  "QT_UITOOLS_LIB"
+  )
+
+# Targets to which this target links.
+SET(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# The include file search paths:
+SET(CMAKE_C_TARGET_INCLUDE_PATH
+  "/home/student/rovi1/RobWork/RobWork/cmake/../ext/boostbindings"
+  "/home/student/rovi1/RobWork/RobWork/cmake/../ext/eigen3"
+  "/home/student/rovi1/RobWork/RobWork/cmake/../src"
+  "/home/student/rovi1/RobWork/RobWork/cmake/../ext/rwyaobi"
+  "/home/student/rovi1/RobWork/RobWork/cmake/../ext/rwpqp"
+  "/home/student/rovi1/RobWork/RobWork/cmake/../ext/lua/src"
+  "/home/student/rovi1/RobWork/RobWork/cmake/../ext/qhull/src"
+  "/home/student/rovi1/RobWork/RobWork/cmake/../ext/unzip"
+  "/home/student/rovi1/RobWork/RobWork/cmake/../ext/assimp/include"
+  "/usr/include/qt4"
+  "/usr/include/qt4/QtOpenGL"
+  "/usr/include/qt4/QtDesigner"
+  "/usr/include/qt4/QtUiTools"
+  "/usr/include/qt4/QtGui"
+  "/usr/include/qt4/QtCore"
+  "/home/student/rovi1/RobWork/RobWorkStudio/cmake/../src"
+  "/home/student/rovi1/RobWork/RobWorkStudio/cmake/../ext/qtpropertybrowser/src"
+  "/usr/include/opencv"
+  )
+SET(CMAKE_CXX_TARGET_INCLUDE_PATH ${CMAKE_C_TARGET_INCLUDE_PATH})
+SET(CMAKE_Fortran_TARGET_INCLUDE_PATH ${CMAKE_C_TARGET_INCLUDE_PATH})
+SET(CMAKE_ASM_TARGET_INCLUDE_PATH ${CMAKE_C_TARGET_INCLUDE_PATH})
